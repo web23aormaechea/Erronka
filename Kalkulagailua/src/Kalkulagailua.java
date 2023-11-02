6ec0d0ef1cc3a4d7ea43f71fa9f07c828b669119
web23mgaray <web23mgaray@imaltuna.com>
2023-11-02 11:44:28 +0100

    Batuketa funtzioa gehituko da.

--- Kalkulagailua/src/Kalkulagailua.java
+++ Kalkulagailua/src/Kalkulagailua.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Kalkulagailua {
     public static void main(String[] args) {
+        batuketa();
+    }
+    private static void batuketa (){
+        int zenbaki1, zenbaki2, emaitza;
+        Scanner eskanerra = new Scanner(System.in);
 
+        System.out.println("===================================================== BATUKETA =========================================================");
+        System.out.println("Sartu lehen zenbakia");
+        zenbaki1 = eskanerra.nextInt();
+        System.out.println("Sartu bigarren zenbakia");
+        zenbaki2 = eskanerra.nextInt();
+        emaitza = zenbaki1 + zenbaki2;
+        System.out.println("Sartutako zenbakien batura " +emaitza+ " da!");
+        System.out.println("========================================================================================================================");
     }
 }
